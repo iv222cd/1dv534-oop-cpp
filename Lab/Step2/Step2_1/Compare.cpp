03d3d96bfeda45faba2065df680fd5696a2c5a87
iv222cd <iv222cd@student.lnu.se>
2018-04-09 22:35:36 +0200

    step2-1: Basic implementation (program should now actually work).

--- Lab/Step2/Step2_1/Compare.cpp
+++ Lab/Step2/Step2_1/Compare.cpp
@@ -2,5 +2,21 @@
 
 bool Compare::equal(const char * string1, const char * string2)
 {
-   return false;
+   bool isEqual = false;
+
+   while ((*string1 != '\0') && (*string2 != '\0'))
+   {
+      if (*string1 != *string2)
+      {
+         break;
+      }
+      *string1++;
+      *string2++;
+   }
+   if ((*string1 == '\0') && (*string2 == '\0'))
+   {
+      isEqual = true;
+   }
+
+   return isEqual;
 }
--- Lab/Step2/Step2_1/MyApp.cpp
+++ Lab/Step2/Step2_1/MyApp.cpp
@@ -8,7 +8,8 @@
 using std::cin;
 using std::cout;
 
-const char* ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+//const char* ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+const char* ALPHABET = "abcdef"; // TODO: remove. Only for testing purpuses.
 
 class MyApp {
 public:
@@ -18,18 +19,58 @@ public:
 void MyApp::run()
 {
    bool isEqual;
+   char userAlphabet[256];
+
+   // Program info
+   std::cout << "\nHow fast do you write?";
+   std::cout << "\n----------------------";
+   std::cout << "\n";
+   std::cout << "\nThis simple program lets you test how fast you write by measuring how fast you can wirte the alphabet.";
 
    // Print instuctions to user.
+   std::cout << "\n";
+   std::cout << "\nInstruction:";
+   std::cout << "\nPress enter, then write all lower case letter in the alphabet in order,";
+   std::cout << "\ni.e. '" << ALPHABET << "'. Then press enter again.";
+   std::cout << "\nThe program will measure the time beteen enter and enter.";
+   std::cout << "\n";
+   std::cout << "\nStart by pressing enter...";
+
+   Clock *clockPtr = new Clock();
+
    // Wait for enter.
+   std::cin.get();
    // Start clock.
+   MyTime timeStart = clockPtr->give_me_the_time();
    // Read line until enter.
+   std::cin.getline(userAlphabet, 256);
    // Stop clock.
-   // Compare.
-   // Show user the result.
+   MyTime timeStop = clockPtr->give_me_the_time();
+   delete clockPtr;
+
    
+   // Compare.
    Compare *comparePtr = new Compare();
-   isEqual = comparePtr->equal(ALPHABET, ALPHABET);
+   isEqual = comparePtr->equal(ALPHABET, userAlphabet);
    delete comparePtr;
+
+   // Show user the result.
+   if (isEqual)
+   {
+      std::cout << "\ncorrect written";
+      std::cout << "\ntime is ...";
+      std::cout << (timeStop - timeStart);
+      std::cout << "\n-----------";
+   }
+   else
+   {
+      std::cout << "\nNot correctly written!";
+      std::cout << "\nYou should have written: " << ALPHABET;
+   }
+
+   // todo: start again (loop).
+   std::cin.get();
+
 }
 
 int main()
