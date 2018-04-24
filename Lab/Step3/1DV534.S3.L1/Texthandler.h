14395ebaee8f3b32ea924226cbde644c08e2ffcb
iv222cd <iv222cd@student.lnu.se>
2018-04-24 20:24:18 +0200

    step3: Adding solution and projects for Step 3 labs. Adding basic files for lab 1.

---
+++ Lab/Step3/1DV534.S3.L1/Texthandler.h
@@ -0,0 +1,14 @@
+#ifndef TEXTHANDLER_H
+#define TEXTHANDLER_H
+
+class Texthandler
+{
+public:
+   Texthandler(const char*, int) {}
+   int antalTexter() { return 0; }
+   int maxAntalTexter() { return 0; }
+   const char *text(int i) { return ""; }
+};
+
+#endif /* TEXTHANDLER_H */
+
