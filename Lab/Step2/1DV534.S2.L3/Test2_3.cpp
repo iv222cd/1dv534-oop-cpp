c5b69465bc1ee3cbf34b621b0d004ba104f24c2e
iv222cd <iv222cd@student.lnu.se>
2018-05-27 11:31:41 +0200

    step2-3: Catching division by zero errors. Throwing TestFailException when incorrect user input. Using stringstream instead of cin in test for more control. More informative messages when exiting.

--- Lab/Step2/1DV534.S2.L3/Test2_3.cpp
+++ Lab/Step2/1DV534.S2.L3/Test2_3.cpp
@@ -24,6 +24,12 @@
 //         2018-04-22   Added more test for better test coverage, by Ingrid Wiklund
 //                      Using exception for testing with the simple TestFailException class.
 //         2018-04-22   Testing that a overflow exception is thrown when dividing with zero.
+//         2018-05-27   Adding a catch for devision with zero.
+//                      Also throwing TestFailException on inccorect input format for fractions intead of just clearing the error.
+//                      This is to give more informative output to user at input errors.
+//         2018-05-27   Replacing cin in Ingrid's testpart with a stringstream,
+//                      so less manual testing is required.
+//         2018-05-27   Replacing std::exception with the less generic std::overflow_error in tests.
 /*************************************************************************/
 
 #include <iostream>
@@ -75,8 +81,7 @@ int Application::run()
    // If error in input
    if (!cin)
    {
-      cin.clear();
-      cin.ignore(INT_MAX, '\n');
+      throw TestFailException("Inccorect input from the user!");
    }
    // ** End section by Ingrid Wiklund ****************
 
@@ -168,7 +173,7 @@ int Application::run()
       Fraction(1, 0); // Devision with zero.
       throw TestFailException("Test Failed: No exception when dividing with zero");
    }
-   catch (const std::exception)
+   catch (const std::overflow_error)
    {
       // All is well. Test passed. Continue...
    }
@@ -180,18 +185,19 @@ int Application::run()
       result = f3 / f4;
       throw TestFailException("Test Failed: No exception when dividing with zero");
    }
-   catch (const std::exception)
+   catch (const std::overflow_error)
    {
       // All is well. Test passed. Continue...
    }
 
    try
    {
-      cout << "Input Fraction with a Zero denominator (i.e. 1/0) -> overflow_error" << endl;
-      cin >> f3;
+      std::stringstream ss("1/0");
+      cout << endl << ss.str() << " >> Fraction -> overflow_error" << endl;
+      ss >> f3;
       throw TestFailException("Test Failed: No exception when dividing with zero");
    }
-   catch (const std::exception)
+   catch (const std::overflow_error)
    {
       // All is well. Test passed. Continue...
    }
@@ -209,18 +215,29 @@ int main()
    try
    {
       returnValue = myApp.run();
-      cout << "All testes passed" << endl;
+      cout << endl << "All testes passed" << endl;
       cin.get();
    }
    catch (TestFailException &e)
    {
-      cout << e.getMessage();
+      cin.clear();
+      cin.ignore(INT_MAX, '\n');
+      cout << endl << "Test failed" << endl;
+      cout << e.getMessage() << endl;
+      cout << "Exiting testprogram" << endl;
       cin.get();
+   }
+   catch (std::overflow_error)
+   {
+      cin.clear();
+      cin.ignore(INT_MAX, '\n');
+      cout << "Division by zero!" << endl;
+      cout << "Exiting testprogram" << endl;
       cin.get();
    }
    catch (...)
    {
-      cout << "Program stopped du to an unknow error" << endl;
+      cout << endl << "Program stopped du to an unknow error" << endl;
       cin.get();
       throw;
    }
